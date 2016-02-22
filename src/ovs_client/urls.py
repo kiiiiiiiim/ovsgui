b395478d8e374fcc1b41b32c55b0fbc5be4e2711
kiiiiiiiim <kim91818824@gmail.com>
2016-02-22 16:06:34 +0900

    address modify

--- src/ovs_client/urls.py
+++ src/ovs_client/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<ovs_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})/(?P<db_name>.*)/(?P<table_name>.*)/$', views.ovsdb_table_data, name='ovsdb_table_data'),
-    url(r'^(?P<ovs_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})/(?P<db_name>.*)/$', views.ovsdb_table_list, name='ovsdb_table_list'),
-    url(r'^(?P<ovs_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})/$', views.ovsdb_list_dbs, name='ovsdb_list_dbs'),
+    url(r'^(?P<ovs_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})/tables/(?P<db_name>.*)/(?P<table_name>.*)/$', views.ovsdb_table_data, name='ovsdb_table_data'),
+    url(r'^(?P<ovs_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})/tables/(?P<db_name>.*)/$', views.ovsdb_table_list, name='ovsdb_table_list'),
+    url(r'^(?P<ovs_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})/tables/$', views.ovsdb_list_dbs, name='ovsdb_list_dbs'),
 ]
